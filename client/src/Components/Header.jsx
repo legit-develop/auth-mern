06dd80cdaf261cec51fc56814d3e98fee2b432dd
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-11-03 15:24:27 +0200

    add content for sign up page

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -14,14 +14,9 @@ export default function Header() {
           <Link to="/about">
             <li>About</li>
           </Link>
-          <Link to="/profile">
-            <li>Profile</li>
-          </Link>
+
           <Link to="/sign-in">
             <li>Sign In</li>
-          </Link>
-          <Link to="/sign-up">
-            <li>Sign Up</li>
           </Link>
         </ul>
       </div>
