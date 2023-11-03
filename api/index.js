f5feb0b3caddee235b2f3e62e01d314cb93ba90e
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-11-03 13:42:03 +0200

    middleware to handle errors

--- api/index.js
+++ api/index.js
@@ -23,3 +23,14 @@ app.listen(3000, () => {
 //api route
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+//middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false, //success: false
+    message, //error: message
+    statusCode, //statusCode:statusCode
+  });
+});
